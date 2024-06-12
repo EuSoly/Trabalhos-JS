288e94fed0dc4d6cf52376d40e25c7a6e3dd3eca
EuSoly <130078074+EuSoly@users.noreply.github.com>
2024-06-12 10:21:10 -0300

    Add files via upload

---
+++ Exercicio-HARD.js
@@ -0,0 +1,80 @@
+// FAÇA UMA FUNÇÃO QUE CALCULE A SOMA, MUTIPLICAÇÃO, DIVISÃO , SUBTRAÇÃO E A MÉDIA DE NUMEROS EM UMA STRING
+const numeros = [[28,5], [97, 38]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Resposta
+
+const numeros = [[20,5], [97, 38]]
+let calc = ["+", "-", "x", "/", "μ"]
+const sinal = calc[4]
+
+function Multiplicacao(num1, num2){
+    resultado = (num1 * num2)
+    return resultado
+}
+function Divisao(num1,num2){
+    resultado = (num1 / num2)
+    return resultado
+}
+function Soma(num1,num2){
+    resultado = (num1 + num2)
+    return resultado
+}
+function Subtracao(num1,num2){
+    resultado = (num1 - num2)
+    return resultado
+}
+function Media(num1,num2){
+    qnt = Media.length
+    resultado = ((num1 + num2) / qnt)
+    return resultado
+}
+
+if (sinal == "+"){
+    console.log("A soma dos numeros solicitados é: ", Soma(numeros[0][0],numeros[1][0]))
+}
+if (sinal == "-"){
+    console.log("A subtração dos numeros solicitados é: ", Subtracao(numeros[0][0],numeros[1][0]))
+}
+if (sinal == "/"){
+    console.log("A divisão dos numeros solicitados é: ", Divisao(numeros[0][0],numeros[1][0]))
+}
+if (sinal == "x"){
+    console.log("A multiplicação dos numeros solicitados é: ", Multiplicacao(numeros[0][0],numeros[1][0]))
+}
+if (sinal == "μ"){
+    console.log("A media dos numeros solicitados é: ", Media(numeros[0][0],numeros[1][0]))
+}
+*/
